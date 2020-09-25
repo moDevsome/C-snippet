94cdbc8484571feb86f745c0a5be487bb7e96190
Mickaël Outhier <contact@mickael-outhier.fr>
2020-09-25 20:45:33 +0200

    Update C#-snippet.cs

--- C#-snippet.cs
+++ C#-snippet.cs
@@ -1,4 +1,9 @@
 //*** Create a GUI window that fill all the screen ***//
-
 Form mainWindow = new Form();
 mainWindow.WindowState = FormWindowState.Maximized;
+
+//*** Set a custom position and size for a GUI window ***//
+ Form mainWindow = new Form();
+mainWindow.Size = new Size(1400, 1050);
+mainWindow.StartPosition = FormStartPosition.Manual;
+mainWindow.DesktopLocation = new Point(0, 0);
