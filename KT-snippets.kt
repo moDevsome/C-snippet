e4488de26881a0b23df1068ff3c1ffb7390f7284
Mickaël Outhier <contact@mickael-outhier.fr>
2020-12-05 19:25:41 +0100

    Update KT-snippets.kt

--- KT-snippets.kt
+++ KT-snippets.kt
@@ -3,3 +3,12 @@ val alertBox = AlertDialog.Builder(this);
 alertBox.setTitle( "App is closing" );
 alertBox.setMessage( "Bye bye" );
 alertBox.show();
+
+// Handle button click
+val closeButton: Button = findViewById(R.id.close_button);
+closeButton.setOnClickListener {
+
+    Log.i("DEBUG","Clique sur le bouton QUITTER");
+    this.sayBye();
+
+}
